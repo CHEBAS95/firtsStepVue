b7c75b199232b77ec557a143b91cdec9891ca14d
chebas95 <sebaszzpinosa@gmail.com>
2024-04-23 01:00:43 -0500

    primer paso en vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>hola Sebillas{{'va a ser un duro en :'+curso }}</h1>
+</template>
+
+<style scoped>
+  h1{
+    background-color: aqua;
+    color: tomato;
+  }
+</style>
+
+<script setup>
+    const curso ='vue'
+</script>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// importaar css desde asset
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
